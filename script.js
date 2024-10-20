fec3350cbcfb52499713caef1e788d1a93a59d70
Aslam <93756533+LW1117@users.noreply.github.com>
2024-10-20 22:27:44 +0530

    Create playGame() to play 5 rounds

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 let humanScore = 0;
 let computerScore = 0;
 
-getComputerChoice = () => {
-  let randomChoice = Math.random();
-  if (randomChoice >= 0 && randomChoice < 1 / 3) {
-    return "rock";
-  } else if (randomChoice >= 1 / 3 && randomChoice < 2 / 3) {
-    return "paper";
-  } else {
-    return "scissors";
+playGame = () => {
+  for (let i = 0; i < 5; i += 1) {
+    console.log("----------------\n");
+    console.log("----------------\n");
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log("Current Score\n");
+    console.log("----------------\n");
+    console.log(`Player: ${humanScore}\n`);
+    console.log(`Computer: ${computerScore}\n`);
   }
-};
-
-getHumanChoice = () => {
-  let choice = prompt("Enter your choice (rock, paper or scissors): ");
-  choice = choice.toLowerCase();
-  if (choice == "rock" || choice == "paper" || choice == "scissors") {
-    return choice;
-  }
-  alert("Wrong Choice!!");
-  return getHumanChoice();
+  if (humanScore > computerScore) alert("You WIN!!!");
+  else alert("You LOST!!!");
 };
 
 playRound = (humanChoice, computerChoice) => {
@@ -47,10 +40,25 @@ playRound = (humanChoice, computerChoice) => {
   return 0;
 };
 
-playRound(getHumanChoice(), getComputerChoice());
+getComputerChoice = () => {
+  let randomChoice = Math.random();
+  if (randomChoice >= 0 && randomChoice < 1 / 3) {
+    return "rock";
+  } else if (randomChoice >= 1 / 3 && randomChoice < 2 / 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
 
-console.log("Current Score\n");
-console.log("----------------\n");
-console.log(`Player: ${humanScore}\n`);
-console.log(`Computer: ${computerScore}\n`);
-console.log("----------------\n");
+getHumanChoice = () => {
+  let choice = prompt("Enter your choice (rock, paper or scissors): ");
+  choice = choice.toLowerCase();
+  if (choice == "rock" || choice == "paper" || choice == "scissors") {
+    return choice;
+  }
+  alert("Wrong Choice!!");
+  return getHumanChoice();
+};
+
+playGame();
